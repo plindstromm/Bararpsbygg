4261586d28a90f9c995ba96a5d6c4553bf479a38
plindstromm <mukkepp@gmail.com>
2026-01-09 08:00:48 +0100

    /w

--- src/components/Cookies/index.tsx
+++ src/components/Cookies/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Cookies from "js-cookie";
-import "./styles.scss"; // Import the SCSS file
+import "./styles.scss"; 
 
 const CookieBanner = () => {
   const [isAccepted, setIsAccepted] = useState(
